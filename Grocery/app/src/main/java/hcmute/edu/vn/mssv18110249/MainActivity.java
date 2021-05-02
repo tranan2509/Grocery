4340748825f3bdad770d536d9c05d9ecebe90183
Tran Van An <tranan2509@gmail.com>
2021-05-02 18:32:53 +0700

    add logo

--- Grocery/app/src/main/java/hcmute/edu/vn/mssv18110249/MainActivity.java
+++ Grocery/app/src/main/java/hcmute/edu/vn/mssv18110249/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,18 +18,36 @@ public class MainActivity extends AppCompatActivity {
     EditText txtEmail, txtPassword;
     Button btnSignIn;
     TextView txtViewForgotPassword, btnSignUp;
-
+    ImageButton imgBtnShow;
+    private boolean isShow = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        txtEmail = (EditText)findViewById(R.id.txtEmail);
-//        txtPassword = (EditText)findViewById(R.id.txtPassword);
-//        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        txtEmail = (EditText)findViewById(R.id.txtEmail);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
         btnSignUp = (TextView) findViewById(R.id.btnSignUp);
         txtViewForgotPassword = (TextView)findViewById(R.id.textViewForgotPassword);
-//
+        imgBtnShow = (ImageButton)findViewById(R.id.btnShow);
+
+        imgBtnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isShow)
+                {
+                    txtPassword.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    isShow = true;
+                }
+                else
+                {
+                    txtPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    isShow = false;
+                }
+            }
+        });
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
